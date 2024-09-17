4adddf431cf3552fd1d96c9ac67c5173afd69f47
HannahVMeyer <hannah.v.meyer@googlemail.com>
2024-09-17 13:10:32 -0400

    add mhc and peptide length to filepath

--- data-processing/scripts/01_generate-fasta.R
+++ data-processing/scripts/01_generate-fasta.R
@@ -4,14 +4,25 @@ library(dplyr)
 library(seqinr)
 
 peptides <- read_csv("data/netmhcpan_inputs_mhc_i.csv")[, -1]
-peptides_list <- split(peptides, f=peptides$mhc)
+
+# format MHC names for netMHCpan
+peptides <- peptides %>%
+  mutate(mhc = case_when(mhc == "H2-Db" ~ "H-2-Db",
+                         mhc == "H2-Kb" ~ "H-2-Kb", 
+        # remove * and : : cluster submission can't deal with these in filenames
+                         TRUE ~ gsub("[\\*:]","", mhc)),
+         length=nchar(peptide),
+         combined = paste0(mhc, "_", length))
+
+peptides_list <- split(peptides, f=peptides$combined)
+
 
 tt <- lapply(seq_along(peptides_list), function(x) {
-  mhc <- names(peptides_list)[x]
+  combined <- names(peptides_list)[x]
   tmp <- peptides_list[[x]] %>%
-    mutate(id=paste0(mhc, "_", 1:n()))
+    mutate(id=paste0(combined, "_", 1:n()))
   write.fasta(sequences=as.list(tmp$peptide),
               names=tmp$id,
-              file.out = file.path("data", paste0("peptides_", mhc, ".fasta")),
+              file.out = file.path("data", paste0("peptides_", combined, ".fasta")),
               as.string=TRUE)
 })
